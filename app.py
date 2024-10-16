74b40d0964ee3bcf068d9b04bd2021562effc161
haandol <ldg55d@gmail.com>
2024-10-16 06:38:47 +0000

    update log

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 import os
+import sys
+import logging
 from dotenv import load_dotenv
 
-from llama_index.tools.tavily_research import TavilyToolSpec
 from llama_index.core.agent import ReActAgent
-from llama_index.core.memory import ChatMemoryBuffer
-from llama_index.llms.bedrock_converse import BedrockConverse
 from llama_index.core.callbacks import CallbackManager
+from llama_index.core.tools.types import BaseTool
+from llama_index.tools.tavily_research import TavilyToolSpec
+from llama_index.llms.bedrock_converse import BedrockConverse
 from langfuse.llama_index import LlamaIndexCallbackHandler
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler = logging.StreamHandler(sys.stdout)
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 load_dotenv()
 
 
 async def main():
     # setup tools
-    tools = [
+    tools: list[BaseTool] = [
         TavilyToolSpec(api_key=os.environ["TAVILY_API_KEY"]).to_tool_list()[0],
     ]
 
@@ -25,9 +34,6 @@ async def main():
         profile_name=os.environ.get("AWS_PROFILE_NAME", None),
     )
 
-    # setup memory
-    memory = ChatMemoryBuffer.from_defaults()
-
     # setup tracing
     langfuse_callback_handler = LlamaIndexCallbackHandler(
         public_key=os.environ["LANGFUSE_PUBLIC_KEY"],
@@ -37,16 +43,18 @@ async def main():
     callback_manager = CallbackManager([langfuse_callback_handler])
 
     # setup agent
-    agent = ReActAgent(
+    agent = ReActAgent.from_tools(
         tools=tools,
         llm=llm,
-        memory=memory,
         callback_manager=callback_manager,
         verbose=True,
     )
 
-    resp = await agent.achat("NCSoft 의 Lineage M 에 대한 게이머들의 최근 평가는 어때?")
-    print(resp)
+    resp = await agent.achat(
+        "NCSoft 의 최신작과 월드 오브 워크래프트의 차이가 뭐야?"
+        "캐주얼한 게이머에게 어떤 게임이 더 좋을까?"
+    )
+    logger.info(f"=== Response ===\n{resp}")
 
 
 if __name__ == "__main__":
